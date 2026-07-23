0698a90b2ca85abe6f7720fe50299162661d275b
Saketh <sakethyuvraj871@gmail.com>
2026-07-23 18:38:02 -0400

    Initial Commit

---
+++ Code Atlas/Code_AtlasApp.swift
@@ -0,0 +1,32 @@
+//
+//  Code_AtlasApp.swift
+//  Code Atlas
+//
+//  Created by Saketh Kammari  on 7/23/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Code_AtlasApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Code AtlasTests/Code_AtlasTests.swift
@@ -0,0 +1,18 @@
+//
+//  Code_AtlasTests.swift
+//  Code AtlasTests
+//
+//  Created by Saketh Kammari  on 7/23/26.
+//
+
+import Testing
+
+struct Code_AtlasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
